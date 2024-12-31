15c6aba0d1d8772c6fe08eca262c83576dcfa607
Denis Sanchez <desanchezca@gmail.com>
2024-12-30 19:55:36 -0500

    update logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,65 @@
+import { useEffect, useState } from 'react';
 import './App.css'; 
 import logo from './assets/images/mifacturaperublanco.png'; 
-import empresaLogo from './assets/images/milka-removebg-preview.png';
 import { Link } from 'react-router-dom';
+import { isString } from 'lodash';
+
+const LastLogoRefreshToken = "last_logo_refresh"
 
 function App() {
+  const [sessionInfo, setSessionInfo] = useState({
+    Ruc: '',
+    imagenesUrl: '',
+  });
+
+  const [logoUrl, setLogoUrl] = useState("");
+
+  useEffect(() => {
+    fetchSessionInfo();
+  }, []);
+
+  useEffect(() => {
+    if (!isString(sessionInfo.Ruc) || !isString(sessionInfo.imagenesUrl)) {
+      setLogoUrl("");
+      return;
+    }
+
+    if (sessionInfo.Ruc == "" || sessionInfo.imagenesUrl == "") {
+      setLogoUrl("");
+      return;
+    }
+
+    if (refreshLogo()) {
+      fetch(`${sessionInfo.imagenesUrl}/api/empresa/${sessionInfo.Ruc}`);
+      localStorage.setItem(LastLogoRefreshToken, new Date().toISOString())
+    }
+
+    setLogoUrl(`${sessionInfo.imagenesUrl}/media/${sessionInfo.Ruc}/logo.png`)
+  }, [sessionInfo])
+
+  const refreshLogo = () => {
+    const lastRefresh = localStorage.getItem(LastLogoRefreshToken);
+
+    if (lastRefresh == null) {
+      return true;
+    }
+
+    const sincesMs = new Date() - new Date(lastRefresh)
+
+    return Math.floor(sincesMs / (1000 * 60)) > 5;
+  }
+
+  const fetchSessionInfo = async () => {
+    const req = await fetch('/api/auth/sesion');
+
+    if (!req.ok) {
+      return;
+    }
+
+    const data = await req.json();
+    setSessionInfo(data);
+  }
+
   return (
     <div>
       <div className="navbar1">
@@ -12,7 +68,9 @@ function App() {
         </div>
       </div>
       <div className='main'>
-        <img src={empresaLogo} alt="LOGO EMPRESA" className="logo-empresa" />
+        {logoUrl != "" && <div className="navbar-left1">
+          <img src={logoUrl} alt="LOGO EMPRESA" className="logo-empresa" />
+        </div>}
       </div>
       <footer className="footer1">
         <Link to="/productos" className="btn-continue1">CONTINUAR</Link>
