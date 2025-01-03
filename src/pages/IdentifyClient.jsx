425081dc0d5df3fa42705510fde879a2d6c5ab8d
AbedNegoLlanos <negolloverallanos@gmail.com>
2025-01-03 15:59:03 -0500

    cambios en useState

--- src/pages/IdentifyClient.jsx
+++ src/pages/IdentifyClient.jsx
@@ -256,7 +256,7 @@ const IdentifyClient = () => {
                     <div className="teclado-numerico">
                         {[1, 2, 3, 4, 5, 6, 7, 8, 9, '', 0, ''].map((num, index) => (
                         <button key={index} type="button" onClick={() => addNumber(num.toString())}>
-                            {num}
+                            {num}recuerda 
                         </button>
                         ))}
                     </div>
